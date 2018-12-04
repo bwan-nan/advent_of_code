1b2a36a66ceca5942ab78ca8c48b8c26f01fd926
Benjamin WAN NAN WO <bwan-nan@e3r4p13.42.fr>
2018-12-04 21:36:48 +0100

    giving up

--- day1/Part2/2_day1.c
+++ day1/Part2/2_day1.c
@@ -6,7 +6,7 @@
 /*   By: bwan-nan <bwan-nan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:04:01 by bwan-nan          #+#    #+#             */
-/*   Updated: 2018/12/04 12:17:31 by bwan-nan         ###   ########.fr       */
+/*   Updated: 2018/12/04 21:36:25 by bwan-nan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,91 +16,73 @@
 #include <fcntl.h>
 #include "day1_part2.h"
 
-static t_flist		*lstcreate(int frequency, int change)
+static int			ft_intchr(int *tab, int frequency, int len)
 {
-	t_flist	*new_flist;
-	if (!(new_flist = (t_flist *)malloc(sizeof(t_flist))))
-		return (NULL);
-	new_flist->frequency = frequency;
-	new_flist->change = change;
-	new_flist->next = NULL;
-	return (new_flist);
-}
+	int i;
 
-static void			load_flist(t_flist **flist, int frequency, int change)
-{
-	t_flist	*tmp;
-
-	if (*flist)
+	i = 0;
+	while (i < len)
 	{
-		tmp = *flist;
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = lstcreate(frequency, change);
+		if (tab[i] == frequency)
+			return (1);
+		i++;
 	}
+	return (0);
 }
 
 int					main(int ac, char **av)
 {
-	t_flist		*flist;
-	t_flist		**ptr;
-	t_flist		**head;
-	t_flist		**head2;
-	t_flist		**tail;
-	char		**tab;
 	char		*line;
-	int			*new_tab;
+	int			*tmp;
+	int			*tab;
+	int			change;
 	int			frequency;
 	int			fd;
-	int			change;
+	int			i;
+	int			stop;
 
-	flist = NULL;
 	frequency = 0;
-	flist = lstcreate(frequency, change);
-	head = &flist;
+	i = 1;
+	if (!tab)
+	{
+		if (!(tab = (int *)malloc(sizeof(int) * 1)))
+			return (-1);	
+		tab[0] = 0;
+	}
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		while (get_next_line(fd, &line))
 		{
-			ptr = &flist;
 			change = ft_atoi(line);
 			frequency += change;
-			while (*ptr)
+			if (!tmp)
 			{
-				if ((*ptr)->frequency == frequency)
-				{
-					ft_putnbr(frequency);
-					ft_putchar('\n');
-					close(fd);
-					return (0);
-				}
-				(*ptr) = (*ptr)->next;
+				tmp = tab;
+				if (!(tab = (int *)malloc(sizeof(int) * (i + 1))))
+					return (-1);
+				ft_memcpy(tab, tmp, sizeof(tab));
+				tab[i] = frequency;
 			}
-			load_flist(ptr, frequency, change);
+			else
+			{
+				tmp = tab;
+				if (!(tab = (int *)malloc(sizeof(int) * (i + 1))))
+					return (-1);
+				ft_memcpy(tab, tmp, sizeof(*tmp));
+				tab[i] = frequency;
+				free(tmp);
+			}
+			if (ft_intchr(tab, tab[i], i))
+			{
+				ft_putnbr(tab[i]);
+				ft_putchar('\n');
+				return (1);
+			}
+			ft_putnbr(tab[i]);
+			ft_putchar('\n');
 		}
-		close(fd);
-	/*	*tail = *ptr;
-		while (1)
-		{
-			frequency += (*ptr)->frequency;
-			(*head2) = (*head);
-			while (*head2)
-			{
-				if ((*head2)->frequency == frequency)
-				{
-					ft_putnbr(frequency);
-					ft_putchar('\n');
-					close(fd);
-					return (0);
-				}
-				(*head2) = (*head2)->next;
-			}
-			load_flist(tail, frequency, (*ptr)->change);
-			(*ptr) = (*ptr)->next;
-			*tail = (*tail)->next;
-		}*/
+		i++;
 	}
-	ft_putnbr(frequency);
 	return (0);
 }
