69b4617786820fe03ad94fdd3d7e030d68969d30
Benjamin WAN NAN WO <bwan-nan@student.42.fr>
2019-12-04 10:29:59 +0400

    d04

---
+++ 2019/D04/part1.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+def check_order(num)
+  num[0].to_i <= num[1].to_i && num[1].to_i <= num[2].to_i && num[2].to_i <= num[3].to_i && num[3].to_i <= num[4].to_i && num[4].to_i <= num[5].to_i
+end
+
+def at_least_two_adjacent_digits(num)
+  i = 0
+  while i < 5
+    digit1 = num[i]
+    digit2 = num[i + 1]
+    return true if digit1 == digit2
+    i += 1
+  end
+  false
+end
+
+def valid_password(num)
+  check_order(num.to_s) && at_least_two_adjacent_digits(num.to_s)
+end
+
+range = File.open('input.txt').first
+min = range.split('-').first.to_i
+max = range.split('-').last.to_i
+
+count = 0
+for i in min..max
+  if valid_password(i)
+    count += 1
+  end
+end
+
+puts count
---
+++ 2019/D04/part2.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+
+def check_order(num)
+  num[0].to_i <= num[1].to_i && num[1].to_i <= num[2].to_i && num[2].to_i <= num[3].to_i && num[3].to_i <= num[4].to_i && num[4].to_i <= num[5].to_i
+end
+
+def check_occurences(digit1, num)
+  num.count(digit1) == 2
+end
+
+def adjacent_digits(num)
+  i = 0
+  while i < 5
+    digit1 = num[i]
+    digit2 = num[i + 1]
+    if digit1 == digit2
+      return true if check_occurences(digit1, num)
+    end
+    i += 1
+  end
+  false
+end
+
+def valid_password(num)
+  check_order(num.to_s) && adjacent_digits(num.to_s)
+end
+
+range = File.open('input.txt').first
+min = range.split('-').first.to_i
+max = range.split('-').last.to_i
+
+count = 0
+for i in min..max
+  if valid_password(i)
+    count += 1
+  end
+end
+
+puts count
