1528a3ed7625c79982499f438b3f754e64c0cf3f
Benjamin Wan Nan Wo <benjamin@tableonline.fr>
2022-12-04 12:18:19 +0100

    improve day3

--- 2022/d03/day.rb
+++ 2022/d03/day.rb
@@ -4,9 +4,9 @@ chars = rucksacks.map do |r|
   a, b = r.chars.each_slice(r.size / 2).map(&:join)
   a.chars & b.chars
 end.flatten
-p chars.map(&:ord).map {|c| c >= 'a'.ord ? c - ('a'.ord - 1) : c - ('A'.ord - 27)}.sum
+p chars.map(&:ord).sum {|c| c >= 'a'.ord ? c - ('a'.ord - 1) : c - ('A'.ord - 27)}
 
 chars = rucksacks.each_slice(3).map do |a, b, c|
   a.chars & b.chars & c.chars
 end.flatten
-p chars.map(&:ord).map {|c| c >= 'a'.ord ? c - ('a'.ord - 1) : c - ('A'.ord - 27)}.sum
+p chars.map(&:ord).sum {|c| c >= 'a'.ord ? c - ('a'.ord - 1) : c - ('A'.ord - 27)}
