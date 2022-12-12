fd0194f6dc62ff5ed1584090d8dd93ec0bca9a21
Benjamin Wan Nan Wo <benjamin@tableonline.fr>
2022-12-13 02:15:14 +0400

    improve day12 performance

--- 2022/d12/day.rb
+++ 2022/d12/day.rb
@@ -1,25 +1,15 @@
 require 'ap'
 require 'set'
 
-Position = Struct.new(:x, :y) do
-  def eql?(other)
-    x == other.x && y == other.y
-  end
-
-  def hash
-    x * 1000 + y
-  end
-end
 
 class Square
-  attr_reader :letter, :position
+  attr_reader :letter, :x, :y
   attr_accessor :parent
 
-  def initialize(position)
-    @position = position
-    @letter = $hill[position.y][position.x]
-    @letter = 'a' if letter == 'S'
-    @letter = 'z' if letter == 'E'
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @letter = $hill[y][x]
     @parent = nil
   end
 
@@ -32,6 +22,8 @@ class Square
   end
 
   def elevation
+    return 0 if letter == 'S'
+    return 'z'.ord - 'a'.ord if letter == 'E'
     letter.ord - 'a'.ord
   end
 
@@ -41,41 +33,28 @@ class Square
 
   def get_neighbors
     n = [] #left, right, top, down
-    n << Square.new(Position.new(position.x - 1, position.y)) if position.x - 1 >= 0 
-    n << Square.new(Position.new(position.x + 1, position.y)) if position.x + 1 < $row_len
-    n << Square.new(Position.new(position.x, position.y - 1)) if position.y - 1 >= 0
-    n << Square.new(Position.new(position.x, position.y + 1)) if position.y + 1 < $col_len
+    n << Square.new(x - 1, y) if x - 1 >= 0 
+    n << Square.new(x + 1, y) if x + 1 < $grid_width
+    n << Square.new(x, y - 1) if y - 1 >= 0
+    n << Square.new(x, y + 1) if y + 1 < $grid_height
     n
   end
 
-  def x
-    position.x
-  end
-
-  def y
-    position.y
-  end
-
   def can_visit?(neighbor)
-    return false if already_visited?(neighbor)
     neighbor.elevation - elevation <= 1
   end
 
-  def already_visited?(neighbor)
-    $visited.include?(neighbor)
-  end
-
   def end?
-    @_end ||= $end_position.x == x && $end_position.y == y
+    letter == 'E'
   end
   
-  def s_node?
-    @_s_node ||= $s_position.x == x && $s_position.y == y
+  def start?
+    letter == 'a' || letter == 'S'
   end
 end
 
-def get_path(end_point)
-  node = end_point
+def get_path(end_node)
+  node = end_node
   path = []
   while node.parent
     node = node.parent
@@ -84,18 +63,25 @@ def get_path(end_point)
   path
 end
 
-def bfs(start_node)
+def bfs(part)
+  visited = Set.new
   queue = []
-  end_point = nil
+  end_node = nil
 
-  queue << start_node
-  $visited << start_node
+  $hill.each_with_index do |line, j|
+    line.each_with_index do |letter, i|
+      square = Square.new(i, j)
+      queue << square if (part == 1 && letter == 'S') || (part == 2 && square.start?)
+    end
+  end
 
   until queue.empty?
     current_node = queue.shift
 
+    next if visited.include?(current_node)
+    visited << current_node
     if current_node.end?
-      end_point = current_node
+      end_node = current_node
       break
     end
 
@@ -103,43 +89,20 @@ def bfs(start_node)
       if current_node.can_visit?(neighbor)
         neighbor.parent = current_node
         queue << neighbor
-        $visited << neighbor
       end
     end
   end
-  end_point
-end
-
-def get_positions
-  start_pos = []
-  end_pos = nil
-  s_pos = nil
-  $hill.each_with_index do |line, j|
-    line.each_with_index do |letter, i|
-      start_pos << Position.new(i, j) if letter == 'a' || letter == 'S'
-      s_pos = Position.new(i, j) if letter == 'S'
-      end_pos = Position.new(i, j) if letter == 'E'
-    end
-  end
-  [start_pos, s_pos, end_pos]
+  end_node
 end
 
 $hill = File.readlines("input.txt").map {|l| l.strip.chars}
-$row_len = $hill[0].length
-$col_len = $hill.length
-a_positions, $s_position, $end_position = get_positions
+$grid_width = $hill[0].length
+$grid_height = $hill.length
 
-paths = []
-s_path_len = 0
-a_positions.each do |a_position|
-  $visited = Set.new
-  start_node = Square.new(a_position)
-  end_point = bfs(start_node)
-  if end_point
-    path = get_path(end_point)
-    paths << path
-    p path.length if start_node.s_node?
-  end
-end
+end_node = bfs(1)
+path = get_path(end_node)
+p path.length
 
-p paths.sort_by(&:size).first.size
+end_node = bfs(2)
+path = get_path(end_node)
+p path.length
