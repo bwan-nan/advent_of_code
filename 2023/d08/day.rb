4fb829e381f73cf228f7d8a8652c31120fc1109e
Benjamin Wan Nan Wo <benjamin.wan-nan-wo@wttj.co>
2023-12-08 13:57:31 +0100

    day 8

---
+++ 2023/d08/day.rb
@@ -0,0 +1,27 @@
+def compute_steps_to_reach_stop(start, stop, nodes, instructions)
+  current_node = start
+  instructions.cycle.with_index do |current_instruction, steps|
+    return steps if current_node.end_with?(stop)
+    current_node = nodes[current_node][current_instruction]
+  end
+end
+
+lines = File.readlines('input.txt').map {|l| l.strip if !l.strip.empty?}.compact
+instructions = lines.shift.chars
+nodes = {}
+
+lines.map do |line|
+  start, connections = line.split(" = ")
+  connections = connections.gsub(/\(|\)/, "").split(", ")
+  nodes[start] = {"L" => connections[0], "R" => connections[1]}
+end
+
+puts compute_steps_to_reach_stop("AAA", "ZZZ", nodes, instructions)
+
+nodes_to_dig = nodes.keys.select {|node| node.end_with?("A")}
+
+steps_per_node = nodes_to_dig.map do |current_node|
+  compute_steps_to_reach_stop(current_node, "Z", nodes, instructions)
+end
+
+puts steps_per_node.reduce(1) {|acc, n| acc.lcm(n)}
